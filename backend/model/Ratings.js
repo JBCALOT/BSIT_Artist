9187e55fd6556d20ba2ba0a1fed477a721092695
ajsupnet <ashley.jullien@outlook.com>
2022-08-06 04:09:35 +0800

    ratings

--- backend/model/Ratings.js
+++ backend/model/Ratings.js
@@ -1,1 +1,9 @@
-//"ratings" another ratings
+const mongoose = require("mongoose");
+
+const ratingSchema = mongoose.Schema({
+
+},{timestamps: true}
+);
+
+const Ratings = mongoose.model("Ratings", ratingSchema );
+module.exports = Ratings;
